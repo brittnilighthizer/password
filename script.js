bc6b50d338f70b9143fe33bf434f6fe394f38bcc
Brittni Lighthizer <brittnikirsten@gmail.com>
2020-01-26 15:32:04 -0600

    homework

--- script.js
+++ script.js
@@ -1,51 +1,82 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaLower = [];
 var symbolic = ["~", "!", "?", "@", "#", "$", "%","^", "&", "*"];
-var numbers = []
-var passwordLength = 
+var numberic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+// Meat of Application; getting all relevant variables from the user to create desired output
+function generatePassword() {
+  //Enforcing the NUMBER input ; return a number value not a 
+  var passwordLength = parseInt(prompt("How many characters for password length?"));
+  if (passwordLength<8 || passwordLength>128) {
+    alert("Please enter a value between 8 and 128 characters.")
+    writePassword();
+  }
+  //Make sure the value for password lenght is a number
+  if (isNaN(passwordLength)) {
+    alert("Please enter only a number between 8 and 128.")
+    writePassword();
+  }
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log(passwordLength);
 
-  passwordText.value = password;
+  // Boolean for capital or lowercase
+  var userAlpha = confirm("Would you like capital letters?")
+  console.log(userAlpha);
+
+  var userNumeric = confirm("Would you like numbers?")
+  console.log(userNumeric);
+
+  var userSymbol = confirm("Would you like symbols? ")
+  console.log(userSymbol);
+
+  //Variable to hold all the potential characters - from the arrays above
+  var potentialCharacters = [];
+  
+  if (userAlpha) {
+    // Concat ; adding strings/values together -- creating longer arrays
+    potentialCharacters = potentialCharacters.concat(alpha);
+  }
+
+  if (userNumeric) {
+    potentialCharacters = potentialCharacters.concat(numberic);
+  }
+
+  if (userSymbol) {
+    potentialCharacters = potentialCharacters.concat(symbolic);
+  }
+
+  console.log(potentialCharacters);
+  
+  var createdPassword = []; 
+  for (i=0; i<passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * potentialCharacters.length); 
+    //adding an item to an array, 
+    createdPassword.push(potentialCharacters[randomNumber]);
+  }
+
+  console.log(createdPassword.join(""));
+// Returns this as the output to be used as a variable
+  return createdPassword.join(""); 
 
 }
-
-
-// Use somehow? //
-
-// var students = ["Kalani", "Brittni", "Zeb", "Diane", "Francisco", "Dane", "Cody", "Stephen", "James", "Margartita"];
-// for (var i = 0; i < 5; i++) {
-//   var randomStudent = Math.random()*10;
-//   var randomStudentindex = Math.floor(randomStudent);
-//   console.log(students[randomStudentindex]);
-//   var random99 = Math.random()*100;
-
-// }
-
-
-console.log(alpha[random]);
-
-// Creates a random number 0-9
-Math.floor(Math.random() * 10);
-
-
-// Use this somehow to adjust if they DO NOT want capital letters
-alpha.toLowerCase();
-
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
+function copyPassword() {
+  var copyText = document.querySelector("#password");
+  copyText.select();
+  document.execCommand("copy");
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+document.querySelector("#copy").addEventListener("click", copyPassword);
